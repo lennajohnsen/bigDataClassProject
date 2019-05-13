46bb7bb489001663bc64a36fff4486f2f9155c15
lennajohnsen <47152562+lennajohnsen@users.noreply.github.com>
2019-05-13 08:07:29 -0400

    colors

--- js/state_map.js
+++ js/state_map.js
@@ -48,6 +48,10 @@ d3.csv("data/MMresettle_2014-2018.csv", function(data) {
 
 //console.log(usa)
 
+var color = d3.scale.threshold()
+		.domain([0,8000])
+		.range(["#beccae","#b6c7a5", "#afc39c", "#a7be93", "#a0b98a","#98b580","#91b077", "#89ab6e","#82a765", "#7aa25c"]);
+
 var stateShapes = svg.append("g")
     .attr("class", "states")
     .selectAll("path")
@@ -55,6 +59,7 @@ var stateShapes = svg.append("g")
 	.enter().append("path")
 	  .attr("d", path)
     .attr("class", "resettle")
+    .attr("fill", color)
     //.attr("opacity", d => d.properties.diffMMResettled14_18/d.properties.resettledMM2014)
   .on("mouseover", function(d, i) {
           reporter(d);
