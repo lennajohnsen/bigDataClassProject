ff98aa5f91c0289a96f878161556d399d80d0e8e
lennajohnsen <47152562+lennajohnsen@users.noreply.github.com>
2019-05-13 07:23:38 -0400

    Update state_map.js

--- js/state_map.js
+++ js/state_map.js
@@ -176,8 +176,8 @@ function showDetail(d) {
   // change outline to indicate hover state.
   d3.select(this).attr('stroke', 'black');
 
-  firstDiff = d.properties.resettled2014 - d.properties.resetled2018
-  secondDiff = d.properties.resettledMM2014 - d.properties.resetled2018
+  firstDiff = +d.properties.resettled2014 - +d.properties.resetled2018
+  secondDiff = +d.properties.resettledMM2014 - +d.properties.resetled2018
 
   var content = '<span class="name">State: </span><span class="value">' +
                 d.State +
