3c29ca1d6c6826761ff48a0b6cf8f2acc2e2276b
lennajohnsen <47152562+lennajohnsen@users.noreply.github.com>
2019-05-12 23:52:25 -0400

    Update dot_density.js

--- js/dot_density.js
+++ js/dot_density.js
@@ -1,12 +1,12 @@
 //https://observablehq.com/@floledermann/dot-density-maps-with-d3
 
 var svg = d3.select("#id5").append("svg")
-  .attr("width", 1200)
-  .attr("height", 1200);
+  .attr("width", 700)
+  .attr("height", 400);
 
 
-var height = 1200
-var width = 1200
+var height = 700
+var width = 400
 var margin = 20
 var path = d3.geoPath()
 
